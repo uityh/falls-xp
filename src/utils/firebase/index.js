df7af26961c8f1fd35483d98fea4c0e6a54cdf09
Abhishek Khatri <abhishekkhatri98@gmail.com>
2023-02-25 21:57:40 -0500

    chore: link firebase

---
+++ src/utils/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: process.env.FB_API_KEY,
+	authDomain: process.env.FB_AUTH_DOMAIN,
+	projectId: process.env.FB_PROJECT_ID,
+	storageBucket: process.env.FB_STORAGE_BUCKET,
+	messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+	appId: process.env.FB_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default app;
