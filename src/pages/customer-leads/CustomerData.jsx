e3fa4bd755634746e773e106fe25751eef88577b
sidprabhak <sidduprabhak@gmail.com>
2023-03-07 16:31:45 -0500

    WIP - Customer Leads Table

---
+++ src/pages/customer-leads/CustomerData.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Customer from '../../../seeding/data/Customer.json';
+
+function tableDisplay() {
+	return (
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>NAME</TableCell>
+						<TableCell align="right">EMAIL</TableCell>
+						<TableCell align="right">PASSWORD</TableCell>
+						<TableCell align="right">CARD NUMBER</TableCell>
+						<TableCell align="right">SECURE CODE</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{Customer.map((cust) => (
+						<TableRow
+							key={cust.name}
+							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+						>
+							<TableCell component="th" scope="row">
+								{cust.name}
+							</TableCell>
+							<TableCell align="right">{cust.email}</TableCell>
+							<TableCell align="right">{cust.password}</TableCell>
+							<TableCell align="right">{cust.billingInfo.cardNum}</TableCell>
+							<TableCell align="right">{cust.billingInfo.secureCode}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
+
+export default tableDisplay;
