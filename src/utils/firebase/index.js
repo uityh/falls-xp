6cff54d0c1dffda58736331e1d6e75c0d255fa10
jkronyak <56709613+jkronyak@users.noreply.github.com>
2023-03-08 15:26:44 -0500

    added Firebase Storage reference to Firebase config

--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -16,5 +17,7 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
 
 export default app;
