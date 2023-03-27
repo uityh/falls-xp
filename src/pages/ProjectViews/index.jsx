1734513a0f0f264559cc344f25fdcacd215e202c
Erik Anhorn <erikjanhorn@gmail.com>
2023-03-27 12:53:47 -0400

    Started writing tests, corrected github issue which deleted View Projects page code

--- src/pages/ProjectViews/index.jsx
+++ src/pages/ProjectViews/index.jsx
@@ -3,7 +3,7 @@
 /* eslint-disable no-console */
 /* eslint-disable */
 import React, { useCallback, useEffect, useState } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import {
 	Typography,
 	Table,
@@ -11,7 +11,7 @@ import {
 	TableCell,
 	TableHead,
 	TableRow,
-	Button
+	Button,
 } from '@mui/material';
 import { useAuthContext } from 'contexts/Auth';
 import { getProjectByInvolvedId } from 'utils/data/projects';
@@ -31,12 +31,6 @@ export default function ProjectViews() {
 	let headers = [];
 	if (user !== null) {
 		if (user.role === 'admin') {
-			// Need to get all active projects (I think)
-		} else if (user.role === 'operations') {
-			// Need to get any projects they are assigned to, show them info relevant to their job
-		} else if (user.role === 'sales') {
-			// Need to get any projects they have been involved in starting, show sales analytic info
-			headers = ['ID', 'Tasks', 'Status', 'Address', 'Cost', 'Customer Notes', 'Action'];
 			// Gets all active projects
 			// TODO: Put in a connection to view the pictures for each project
 			headers = [
@@ -92,7 +86,7 @@ export default function ProjectViews() {
 		if (user.role === 'operations') {
 			// Gets any projects they are assigned to, shows relevant info and pictures
 			// TODO: Put in a connection to view the pictures for each project
-			headers = ['ID', 'Tasks', 'Status', 'Address', 'Cost', 'Customer Notes', 'Photos'];
+			headers = ['ID', 'Tasks', 'Status', 'Address', 'Cost', 'Customer Notes'];
 			return (
 				<div>
 					<Typography className="body-text">
@@ -126,11 +120,6 @@ export default function ProjectViews() {
 										{project.cost === 0 ? 'TBD' : project.cost}
 									</TableCell>
 									<TableCell>{project.customerNotes}</TableCell>
-									{
-										project.tasks.includes('initial inspection') 
-										? <TableCell><Link to={`/photo-review/${project.id}`}><Button variant='contained'>View Photos</Button></Link></TableCell> :
-										<TableCell></TableCell>
-									}
 								</TableRow>
 							))}
 						</TableBody>
@@ -220,8 +209,47 @@ export default function ProjectViews() {
 					</Table>
 				</div>
 			);
-		} else if (user.role === 'customer') {
-			// Need to show projects they are customers of, maybe allowed to see pics, otheriwse just progress
+		}
+		if (user.role === 'customer') {
+			// Shows projects they are customers of, shwos project progress
+			headers = ['ID', 'Status', 'Address', 'Cost'];
+			return (
+				<div>
+					<Typography className="body-text">
+						Current User: {user.firstName}
+					</Typography>
+					<Typography className="body-text">Role: {user.role}</Typography>
+					<Typography
+						className="body-heading"
+						sx={{ fontWeight: 'bold', fontSize: '22px' }}
+					>
+						Projects:{' '}
+					</Typography>
+					<Table sx={{ p: 2 }}>
+						<TableHead>
+							<TableRow>
+								{headers.map((headCell) => (
+									<TableCell key={headCell} sx={{ fontWeight: 'bold' }}>
+										{headCell}
+									</TableCell>
+								))}
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{involvedProjects.map((project) => (
+								<TableRow key={project.id}>
+									<TableCell>{project.id}</TableCell>
+									<TableCell>{project.status}</TableCell>
+									<TableCell>{project.address}</TableCell>
+									<TableCell>
+										{project.cost === 0 ? 'TBD' : project.cost}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</div>
+			);
 		}
 	}
 	return (
